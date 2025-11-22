48dd9ecef032f286bf006f8ab6b6753258a32ed1
Oshlook <kitayev.evgeni@gmail.com>
2025-11-22 14:11:19 +0300

    Task06

---
+++ Task06/data_add.sql
@@ -0,0 +1,62 @@
+INSERT OR IGNORE INTO users (name, email, gender, register_date, occupation_id)
+VALUES
+('Китаев Евгений', 'evgenikitaev@gmail.com', 'male', date('now'),
+ (SELECT id FROM occupations ORDER BY id LIMIT 1)),
+('Кармазов Никита', 'karmaz@gmail.com', 'male', date('now'),
+ (SELECT id FROM occupations ORDER BY id LIMIT 1)),
+('Ермаков Егор', 'egor.ermak@gmail.com', 'male', date('now'),
+ (SELECT id FROM occupations ORDER BY id LIMIT 1)),
+('Данькин Иван', 'dankin@gmail.com', 'male', date('now'),
+ (SELECT id FROM occupations ORDER BY id LIMIT 1)),
+('Копеин Александер', 'sanya@gmail.com', 'male', date('now'),
+ (SELECT id FROM occupations ORDER BY id LIMIT 1));
+
+
+
+INSERT OR IGNORE INTO movies (title, year)
+VALUES
+('Армагеддон (1998)', 1998),
+('Иллюзия обмана (2013)', 2013),
+('Аватар (2009)', 2009);
+
+
+INSERT OR IGNORE INTO genres (name) VALUES ('Action');
+INSERT OR IGNORE INTO genres (name) VALUES ('Thriller');
+INSERT OR IGNORE INTO genres (name) VALUES ('Adventure');
+
+INSERT OR IGNORE INTO movie_genres (movie_id, genre_id)
+SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Western'
+WHERE m.title = 'Армагеддон (1998)';
+
+INSERT OR IGNORE INTO movie_genres (movie_id, genre_id)
+SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Fantasy'
+WHERE m.title = 'Иллюзия обмана (2013)';
+
+INSERT OR IGNORE INTO movie_genres (movie_id, genre_id)
+SELECT m.id, g.id FROM movies m JOIN genres g ON g.name = 'Adventure'
+WHERE m.title = 'Аватар (2009)';
+
+
+INSERT INTO ratings (user_id, movie_id, rating, timestamp)
+SELECT u.id, m.id, 4.9, strftime('%s','now')
+FROM users u JOIN movies m ON m.title = 'Армагеддон (1998)'
+WHERE u.email = 'evgenikitaev@gmail.com'
+AND NOT EXISTS (
+    SELECT 1 FROM ratings r WHERE r.user_id = u.id AND r.movie_id = m.id
+);
+
+INSERT INTO ratings (user_id, movie_id, rating, timestamp)
+SELECT u.id, m.id, 5.0, strftime('%s','now')
+FROM users u JOIN movies m ON m.title = 'Иллюзия обмана (2013)'
+WHERE u.email = 'evgenikitaev@gmail.com'
+AND NOT EXISTS (
+    SELECT 1 FROM ratings r WHERE r.user_id = u.id AND r.movie_id = m.id
+);
+
+INSERT INTO ratings (user_id, movie_id, rating, timestamp)
+SELECT u.id, m.id, 4.8, strftime('%s','now')
+FROM users u JOIN movies m ON m.title = 'Аватар (2009)'
+WHERE u.email = 'evgenikitaev@gmail.com'
+AND NOT EXISTS (
+    SELECT 1 FROM ratings r WHERE r.user_id = u.id AND r.movie_id = m.id
+);
